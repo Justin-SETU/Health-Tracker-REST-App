7b8dc5e0234069ae5082f36d305198827688f096
Justin-SETU <20110497@mail.wit.ie>
2024-10-15 14:44:16 +0100

    All files added

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -1,4 +1,33 @@
 package ie.setu.controllers
 
-class HealthTrackerController {
+import ie.setu.domain.repository.UserDAO
+import io.javalin.http.Context
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+import ie.setu.domain.User
+
+object HealthTrackerController {
+
+    private val userDao = UserDAO()
+
+    fun getAllUsers(ctx: Context) {
+        ctx.json(userDao.getAll())
+    }
+
+    fun getUserByUserId(ctx: Context) {
+        val user = userDao.findById(ctx.pathParam("user-id").toInt())
+        if (user != null) {
+            ctx.json(user)
+        }
+    }
+
+    fun addUser(ctx: Context) {
+        val mapper = jacksonObjectMapper()
+        val user = mapper.readValue<User>(ctx.body())
+        userDao.save(user)
+        ctx.json(user)
+    }
+
 }
